a1b41198bb554c29137c0a6929bd754984e4d3ff
chloepanelewen <124078506+chloepanelewen@users.noreply.github.com>
2023-05-11 11:14:24 -0600

    Update and rename Scripts/MoveDown.cs to RepeatBackground.cs

--- RepeatBackground.cs
+++ RepeatBackground.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoveDown : MonoBehaviour
+public class RepeatBackground : MonoBehaviour
 {
     public float speed;
     public float normalSpeed = 3;
